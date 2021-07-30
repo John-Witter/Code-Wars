2b3e7ffb873dadbc1d6b443ae3b284c76e4e3111
John-Witter <mrsolginski@gmail.com>
2021-07-29 23:11:36 -0400

    uniqueInOrder.js

---
+++  Unique In Order/uniqueInOrder.js
@@ -0,0 +1,32 @@
+var uniqueInOrder = function (iterable) {
+    if (!iterable.length) return []
+    //your code here - remember iterable can be a string or an array
+    let rtnArr = [iterable[0]]
+    let initEl = iterable[0]
+    for (let i = 1; i < iterable.length; i++) {
+        let currEl = iterable[i]
+        if (currEl === initEl) {
+            if (iterable[i + 1] && currEl === iterable[i + 1])
+                continue
+        }
+        else {
+            rtnArr.push(currEl)
+            initEl = currEl
+        }
+    }
+
+    //   console.log(rtnArr)  
+    return rtnArr
+}
+
+console.log(uniqueInOrder('AAAABBBCCDAABBB'))
+console.log(uniqueInOrder('ABBCcAD'))
+console.log(uniqueInOrder([1, 2, 2, 3, 3]))
+console.log(uniqueInOrder([]))
+console.log(uniqueInOrder(''))
+
+
+// uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+// uniqueInOrder('ABBCcAD') == ['A', 'B', 'C', 'c', 'A', 'D']
+// uniqueInOrder([1, 2, 2, 3, 3]) == [1, 2, 3]
+// uniqueInOrder([1, 2, 2, 3, 3]) == []
